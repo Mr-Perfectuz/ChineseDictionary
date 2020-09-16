a15a57727dcbf80aa71e56d6e59acda33ec264fc
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-17 04:01:33 +0500

    BookmarkFragment  has been added

--- app/src/main/java/mrperfect/example/chinesedictionary/DictionaryFragment.java
+++ app/src/main/java/mrperfect/example/chinesedictionary/DictionaryFragment.java
@@ -1,16 +1,12 @@
-package mrperfect.example.chinesedictionary.ui;
+package mrperfect.example.chinesedictionary;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import mrperfect.example.chinesedictionary.R;
+import androidx.fragment.app.Fragment;
 
 
 public class DictionaryFragment extends Fragment {
